a01cdf02e894ac1260332bf29e2f32763eefabca
JoaquinBassano <joaquinbas98@gmail.com>
2021-11-08 15:49:09 -0300

    Include js

--- Rock Paper Scissors Web Application/main.go
+++ Rock Paper Scissors Web Application/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
+	"myapp/rps"
 	"net/http"
 )
 
@@ -10,8 +12,21 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html")
 }
 
+func playRound(w http.ResponseWriter, r *http.Request) {
+	result := rps.PlayRound(1)
+
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
 func main() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/play", playRound)
 
 	log.Println("Starting web server on port 8080")
 	http.ListenAndServe(":8080", nil)
